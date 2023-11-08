bd0f8b7deceb3e47c9d469f0347e811a7575ee14
donndewey <donn@deweydata.io>
2023-11-07 16:26:49 -0800

    package name change

---
+++ R/dewedatayr_package.R
@@ -0,0 +1,17 @@
+#' deweyr: An R package for Dewey Data Inc.
+#'
+#' This package includes helper functions for Dewey Data.
+#'
+#' @docType package
+#' @name deweydatar-package
+#' @note
+#' This is a beta version.
+#'
+#' @author
+#' Dewey Data Inc.
+#'
+#' @references
+#'
+#' None
+
+NULL
